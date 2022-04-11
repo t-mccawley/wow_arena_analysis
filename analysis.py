ebeee7d608472395aa19c9eb10d492d315f9536c
Travis McCawley <travis.r.mccawley@gmail.com>
2022-04-10 18:31:11 -0700

    get full plot working

--- analysis.py
+++ analysis.py
@@ -1,13 +1,12 @@
-from audioop import reverse
 import json
-import struct
 import numpy as np
+import matplotlib.pyplot as plt
 
 # file paths
 playable_classes_fp = "data\playable_classes_index.json"
 playable_races_fp = "data\playable_races_index.json"
-# leaderboard_fp = "data\leaderboard.json"
-leaderboard_fp = "data\leaderboard_partial.json"
+leaderboard_fp = "data\leaderboard.json"
+# leaderboard_fp = "data\leaderboard_partial.json"
 
 # helper classes and functions
 class TeamData:
@@ -42,26 +41,27 @@ def _determine_team_comp(team_data_raw,verbosity=0):
         print('\tTeam Games Played: {}'.format(team_games_played))
         print('\tMembers:')
     valid_member_list = []
-    for member_data in team_data_raw['team']['members']:
-        member_name = member_data['character']['name']
-        member_class_id = member_data['character']['playable_class']['id']
-        member_class = playable_classes_dict[member_class_id]
-        member_race_id = member_data['character']['playable_race']['id']
-        member_race = playable_races_dict[member_race_id]
-        member_games_played = member_data['season_match_statistics']['played']
-        if member_class_id != 0 and member_games_played > 0:
-            valid_member_list.append((member_games_played,member_name,member_class))
-        if verbosity == 2:
-            print('\t\tName: {}'.format(member_name))
-            print('\t\tClass: {}'.format(member_class))
-            print('\t\tRace: {}'.format(member_race))
-            print('\t\tGames Played: {}'.format(member_games_played))
-            print('\t\tFraction Games Played: {:0.0f}%'.format(member_games_played/team_games_played*100))
-            print()
     comp = None
-    if len(valid_member_list) >= 2:
-        sorted_valid_member_list = sorted(valid_member_list,reverse=True)
-        comp = _create_comp_name(sorted_valid_member_list[0][2],sorted_valid_member_list[1][2])
+    if 'members' in team_data_raw['team']:
+        for member_data in team_data_raw['team']['members']:
+            member_name = member_data['character']['name']
+            member_class_id = member_data['character']['playable_class']['id']
+            member_class = playable_classes_dict[member_class_id]
+            member_race_id = member_data['character']['playable_race']['id']
+            member_race = playable_races_dict[member_race_id]
+            member_games_played = member_data['season_match_statistics']['played']
+            if member_class_id != 0 and member_games_played > 0:
+                valid_member_list.append((member_games_played,member_name,member_class))
+            if verbosity == 2:
+                print('\t\tName: {}'.format(member_name))
+                print('\t\tClass: {}'.format(member_class))
+                print('\t\tRace: {}'.format(member_race))
+                print('\t\tGames Played: {}'.format(member_games_played))
+                print('\t\tFraction Games Played: {:0.0f}%'.format(member_games_played/team_games_played*100))
+                print()
+        if len(valid_member_list) >= 2:
+            sorted_valid_member_list = sorted(valid_member_list,reverse=True)
+            comp = _create_comp_name(sorted_valid_member_list[0][2],sorted_valid_member_list[1][2])
     
     team_data = TeamData(team_name,rating,rank,comp)
     if verbosity == 1:
@@ -88,30 +88,27 @@ playable_races_dict[0] = "None"
 f.close()
 
 # construct leaderboard data
-f = open(leaderboard_fp)
+f = open(leaderboard_fp, encoding="utf8")
 # print(f)
 data = json.load(f)
 team_data_dict = {}
 for team_data_raw in data['entries']:
-    team_name, team_data = _determine_team_comp(team_data_raw,verbosity=1)
+    team_name, team_data = _determine_team_comp(team_data_raw,verbosity=0)
     team_data_dict[team_name] = team_data
 f.close()
 
 # calculate statistics and plot
 rating_bracket_dict = {} # key of rating floored to nearest 100 with value of list of TeamData objects
-for rating in np.arange(0,3100,100):
+rating_brackets = np.arange(0,3100,100)
+for rating in rating_brackets:
     rating_bracket_dict[rating] = []
 for team_name in team_data_dict:
     team_data = team_data_dict[team_name]
     rating_key = int(np.floor(team_data.rating/100)*100)
     rating_bracket_dict[rating_key].append(team_data)
-
-# get all unique comps
-
-# scan all comps and rating brackets to compute fraction of comp in each rating bracket
-
 # compute count of team comps in each rating bucket
 rating_comp_dict = {} # {rounded_rating, {comp,percent}}
+comp_dict = {}
 for rating_bracket in rating_bracket_dict:
     # check for init
     if rating_bracket not in rating_comp_dict:
@@ -122,9 +119,39 @@ for rating_bracket in rating_bracket_dict:
         # check for init
         if team_comp not in rating_comp_dict[rating_bracket]:
             rating_comp_dict[rating_bracket][team_comp] = 0
+        if team_comp not in comp_dict:
+            comp_dict[team_comp] = True
         # count
         rating_comp_dict[rating_bracket][team_comp] += 1
+    # normalize
     for team_comp in rating_comp_dict[rating_bracket]:
         rating_comp_dict[rating_bracket][team_comp] /= rating_bracket_count
         rating_comp_dict[rating_bracket][team_comp] *= 100
-print(rating_comp_dict)
+# print(rating_comp_dict)
+print(comp_dict.keys())
+# plot
+plt.figure()
+from itertools import cycle
+lines = ["-","--","-.",":"]
+linecycler = cycle(lines)
+for team_comp in comp_dict:
+    if team_comp == None:
+        continue
+    comp_occurrence = []
+    for rating_bracket in rating_brackets:
+        if team_comp not in rating_comp_dict[rating_bracket]:
+            val = 0
+        else:
+            val = rating_comp_dict[rating_bracket][team_comp]
+        comp_occurrence.append(val)
+    if max(comp_occurrence) >= 1.0:
+        plt.plot(rating_brackets,comp_occurrence,label=team_comp,linestyle=next(linecycler))
+
+plt.legend(loc='upper left')
+plt.xlabel('Rating')
+plt.ylabel('Comp Frequency [%]')
+plt.xticks(rating_brackets,rotation=45)
+plt.xlim(1500,2600)
+plt.ylim(0,7.5)
+plt.grid()
+plt.show()
